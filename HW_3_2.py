2863231133b0feab52b5eec1f108536371af3fb4
Oksana Herzhan <oksanafomenko.n@gmail.com>
2024-03-17 23:50:53 +0200

    v_2

--- HW_3_2.py
+++ HW_3_2.py
@@ -1,8 +1,26 @@
 import random
-numbers = list(range(1, 51))
+def get_numbers_ticket(min:int, max:int ,quantity:int) -> list:
+    lot_number = list()
+    for i in range(min,max):
+        lot_number.append(i)
+    lot_choices = random.sample(lot_number,k=quantity)
+    return lot_choices
 
-def get_numbers_ticket():
-    ticket_numbers = random.sample(numbers, 5)
-    return sorted(ticket_numbers)
+incur_input = True
+while incur_input:
+    try:
+        min_str, max_str, quantity_str = input("Input min, max and qty: ").split()
+        min_value = int(min_str)
+        max_value = int(max_str)
+        quantity = int(quantity_str)  
+        if 1<=min_value<max_value and max_value <=1000 and quantity> 0 and quantity<=(max_value-min_value):
+            incur_input = False
+        else: 
+            raise ValueError
+    except ValueError:
+        print("Incorect data input. Use positive integer numbers, min should be less than max, quantity more than 0")
+        incur_input = True
 
-print(get_numbers_ticket())
+lottery_numbers = get_numbers_ticket(min_value,max_value,quantity)
+
+print(f"Your numbers is: {lottery_numbers}")
